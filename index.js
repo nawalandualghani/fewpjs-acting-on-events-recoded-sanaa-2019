ef062c928f6aa82e81cdc74d7fd0ff442938be76
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-17 12:04:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ dodger.style.bottom = "0px";
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
- 
+
   if (left > 0) {
     dodger.style.left = `${left - 1}px`;
   }
