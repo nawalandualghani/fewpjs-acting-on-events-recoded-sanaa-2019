9c154ca11df9778f7e62e195cd337191e9621869
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-17 12:04:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 let dodger = document.getElementById('dodger');
 dodger.style.backgroundColor = "#FF69B4";
 dodger.style.bottom = "0px";
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
